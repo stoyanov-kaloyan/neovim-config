20356740387b2e2dfcc78b35a065f3087c3ac43a
stoyanov-kaloyan <kaloyanstoyanov113@gmail.com>
2026-04-04 00:52:34 +0300

    add lean and improve vimtex config :D real math genius

--- lua/kale/lazy/vimtex.lua
+++ lua/kale/lazy/vimtex.lua
@@ -1,10 +1,19 @@
 return {
     "lervag/vimtex",
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
+    lazy = false,
     init = function()
         vim.g.vimtex_view_method = "general"
         vim.g.vimtex_view_general_viewer = "/mnt/c/Users/kaloy/AppData/Local/SumatraPDF/SumatraPDF.exe"
         vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+
+        vim.g.vimtex_compiler_latexmk = {
+            executable = "latexmk",
+            options = {
+                "-pdf",
+                "-interaction=nonstopmode",
+                "-synctex=1",
+            },
+            out_dir = "build",
+        }
     end,
 }
